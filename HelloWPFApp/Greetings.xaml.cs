74b1656d2b1715b363724db26d607a8ac4bd5483
Your Name <you@example.com>
2021-01-30 16:26:11 -0600

    Mi primer proyecto en WPF

---
+++ HelloWPFApp/Greetings.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace HelloWPFApp
+{
+    /// <summary>
+    /// Lógica de interacción para Greetings.xaml
+    /// </summary>
+    public partial class Greetings : Window
+    {
+        public Greetings()
+        {
+            
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, RoutedEventArgs e)
+        {
+            if (HelloButton.IsChecked == true)
+            {
+                MessageBox.Show("Hello. Mauro");
+            }
+            else if (GoodbyeButton.IsChecked == true)
+            {
+                MessageBox.Show("Goodbye. Mauro");
+            }
+        }
+    }
+}
